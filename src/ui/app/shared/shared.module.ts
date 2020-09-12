e0a1cc816a0659060134baca79c6250b65006fc4
Simon LECUTIEZ <simon.lecutiez@gmail.com>
2020-09-12 18:02:00 +0200

    refactor: Remove default Angular webpage 🗑️

--- src/ui/app/shared/shared.module.ts
+++ src/ui/app/shared/shared.module.ts
@@ -3,13 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { TranslateModule } from '@ngx-translate/core';
 
-import { PageNotFoundComponent } from './components/';
 import { WebviewDirective } from './directives/';
 import { CamelCasePipe } from './pipes/';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [PageNotFoundComponent, WebviewDirective, CamelCasePipe],
+  declarations: [WebviewDirective, CamelCasePipe],
   imports: [CommonModule, TranslateModule, FormsModule],
   exports: [TranslateModule, WebviewDirective, CamelCasePipe, FormsModule],
   providers: [CamelCasePipe]
